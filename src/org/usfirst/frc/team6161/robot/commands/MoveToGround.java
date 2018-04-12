3031d96c5c3fca5b9122974d9ddf85c1b862aa9e
Student <Student@10.10.104.255>
2018-04-12 15:34:28 -0400

    Outtake Preset Works while MoveToGround doesn't

--- src/org/usfirst/frc/team6161/robot/commands/MoveToGround.java
+++ src/org/usfirst/frc/team6161/robot/commands/MoveToGround.java
@@ -20,6 +20,7 @@ public class MoveToGround extends CommandGroup {
 	        // e.g. if Command1 requires chassis, and Command2 requires arm,
 	        // a CommandGroup containing them would require both the chassis and the
 	        // arm.
+		 
 		 	addSequential(new SlideDownwards());
 	    	addSequential(new SlideForwards()); 
 	    
